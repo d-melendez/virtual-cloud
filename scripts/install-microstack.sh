99445d8a0f5a9d4637d51cb742421b32b7a65c21
David Meléndez <daalmega@gmail.com>
2025-09-14 11:29:37 -0500

    fix dev

--- scripts/install-microstack.sh
+++ scripts/install-microstack.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 echo "==> Installing MicroStack"
 
 sudo apt update -y \
-    && sudo apt install -y snapd python3 python3-dev nginx \
+    && sudo apt install -y snapd python3 python3-dev \
     && sudo apt clean && sudo apt autoremove -y
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-sudo snap install microstack --beta
+sudo snap install microstack --devmode --beta
 if [ $? -ne 0 ]; then
     exit 1
 fi
