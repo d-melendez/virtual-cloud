b1f18b68995fa565704d1ce2c1ab548edbfd2d2e
David Meléndez <daalmega@gmail.com>
2025-09-14 09:58:08 -0500

    fix syntax

--- scripts/10-install-microstack.sh
+++ scripts/10-install-microstack.sh
@@ -5,7 +5,7 @@ echo "==> Installing MicroStack"
 
 sudo apt update -y \
     && sudo apt install -y snapd python3 python3-dev nginx \
-    && sudo apt clean && sudo apt autoremove -y \
+    && sudo apt clean && sudo apt autoremove -y
 if [ $? -ne 0 ]; then
     exit 1
 fi
