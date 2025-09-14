02df60d56a4d5ed0a7c3b8375479e8811cc45cda
David Meléndez <daalmega@gmail.com>
2025-09-14 08:43:58 -0500

    update install microstack

--- scripts/10-install-microstack.sh
+++ scripts/10-install-microstack.sh
@@ -4,13 +4,23 @@ set -euo pipefail
 echo "==> Installing MicroStack"
 
 sudo apt update -y \
-    && sudo apt install snapd -y \
+    && sudo apt install snapd python python-dev -y \
     && sudo apt clean && sudo apt autoremove -y
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 sudo snap install microstack --beta
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+sudo 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+sudo microstack init --auto --control
 if [ $? -ne 0 ]; then
     exit 1
 fi
