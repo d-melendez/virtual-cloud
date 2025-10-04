36d4413fa0383fbb944591e23423ee6d62250ceb
David Meléndez <daalmega@gmail.com>
2025-10-04 13:21:23 -0500

    set restrictions again

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -8,6 +8,21 @@ function add_new_project() {
     if [[ $? -ne 0 ]]; then
         exit 1
     fi
+
+    incus project set $project_name limits.containers=5
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+
+    incus project set $project_name limits.cpu=2
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+
+    incus project set $project_name limits.memory=2GB
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
 }
 
 project_name=$1
