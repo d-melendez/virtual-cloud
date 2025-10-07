9ca6419b1d1206dfdd45f510681ffde959b9b1be
David Meléndez <daalmega@gmail.com>
2025-10-07 18:26:08 -0500

    remove restrictions

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -4,25 +4,7 @@ set -ou pipefail
 
 function add_new_project() {
     project_name=$1
-    incus project create $project_name #--config restricted=true
-    if [[ $? -ne 0 ]]; then
-        exit 1
-    fi
-
-    incus project set $project_name limits.containers=5
-    if [[ $? -ne 0 ]]; then
-        exit 1
-    fi
-
-    incus project set $project_name limits.cpu=2
-    if [[ $? -ne 0 ]]; then
-        exit 1
-    fi
-
-    incus project set $project_name limits.memory=2GB
-    if [[ $? -ne 0 ]]; then
-        exit 1
-    fi
+    incus project create $project_name
 }
 
 project_name=$1
@@ -33,7 +15,7 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-incus config trust add $identity --projects $project_name --restricted
+incus config trust add $identity --projects $project_name #--restricted
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
