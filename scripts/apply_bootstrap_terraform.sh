233d0c9cf725d1d91bf50c046340ef086fb00808
David Meléndez <daalmega@gmail.com>
2025-10-24 00:56:14 -0500

    check err

--- scripts/apply_bootstrap_terraform.sh
+++ scripts/apply_bootstrap_terraform.sh
@@ -21,3 +21,9 @@ terraform apply -auto-approve \
   -var "auth0_client_id=$auth0_client_id" \
   -var "auth0_client_secret=$auth0_client_secret" \
   -var "incus_ip_address=$incus_ip_address"
+if [[ $? -ne 0 ]]; then
+    echo "failed to apply terraform"
+    exit 1
+fi
+
+exit 0
