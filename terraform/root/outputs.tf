727561e4f3855e7e2021c09962e8b888b94227d2
David Meléndez <daalmega@gmail.com>
2025-10-24 00:42:37 -0500

    remove output

--- terraform/root/outputs.tf
+++ terraform/root/outputs.tf
@@ -2,8 +2,3 @@ output "auth0_application_client_id" {
   description = "Auth0 application (client) ID"
   value       = module.incus_auth0_app.client_id
 }
-
-output "auth0_application_domain" {
-  description = "Auth0 application domain"
-  value       = module.incus_auth0_app.domain
-}
