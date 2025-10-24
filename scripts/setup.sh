3d1c487a1050f9b826d8315c2f43846db594a49e
David Meléndez <daalmega@gmail.com>
2025-10-24 01:13:47 -0500

    don't source scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,19 +11,19 @@ incus_ip_address="${4}:8443"
 # go to the root of the repository
 cd "$(git rev-parse --show-toplevel)"
 
-. ./scripts/setup_terraform.sh
+bash scripts/setup_terraform.sh
 if [[ $? -ne 0 ]]; then
     echo "failed to setup terraform"
     exit 1
 fi
 
-bash ./scripts/apply_bootstrap_terraform.sh "$auth0_domain" "$client_id" "$client_secret" "$incus_ip_address"
+bash scripts/apply_bootstrap_terraform.sh "$auth0_domain" "$client_id" "$client_secret" "$incus_ip_address"
 if [[ $? -ne 0 ]]; then
     echo "failed to apply bootstrap terraform"
     exit 1
 fi
 
-. ./scripts/setup_incus_server.sh "$auth0_domain"
+bash scripts/setup_incus_server.sh "$auth0_domain"
 if [[ $? -ne 0 ]]; then
     echo "failed to setup server"
     exit 1
